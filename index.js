4239a0910ea5319c717200fc1f5b62ef08f5f0e3
Elen <lielena412@gmail.com>
2025-04-10 23:37:34 +0900

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const button = document.querySelector("button");
-let list = document.querySelector('ul');
-button.addEventListener("click", function () {
-    document.querySelector('.start').style.display = 'block';
+let list = document.querySelector(".users");
+let start = document.querySelector(".start");
+let error = document.querySelector(".error");
+
+try {
+  button.addEventListener("click", function () {
+    list.style.display = "none";
+    button.style.display = "none";
+    setTimeout(() => {
+      button.style.display = "block";
+    }, 3000);
+    list.innerHTML = "";
+    start.style.display = "block";
     setTimeout(async () => {
-        let responce = await fetch('https://jsonplaceholder.typicode.com/users'
+      try {
+        let responce = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
         );
         let users = await responce.json();
-    },2000)  
-    
-    for(let user of users) {
-        list.innerHTML = 
-    }
-});
+        for (let user of users) {
+          const item = document.createElement("li");
+          item.textContent = user.name;
+          list.appendChild(item);
+        }
+        start.style.display = "none";
+        list.style.display = "block";
+      } catch (fetchError) {
+        error.innerHTML = "Ошибка при загрузке данных: " + fetchError.message;
+      }
+    }, 2000);
+  });
+} catch (mainError) {
+  error.innerHTML = "Ошибка: " + mainError.message;
+}
